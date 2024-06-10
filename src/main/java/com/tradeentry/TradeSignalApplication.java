c7dac3d58354c19f2c7f6f6ab19388f2e202c1d5
leandrodalbo <leandro.e.dal.bo@gmail.com>
2024-06-10 14:28:04 +0100

    initial commit

---
+++ src/main/java/com/tradeentry/TradeSignalApplication.java
@@ -0,0 +1,13 @@
+package com.tradeentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSignalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeSignalApplication.class, args);
+	}
+
+}
